fa9249e5fddaba0b9d06fd2c2a15b48053fa9bc4
Tonexus <ual_ynot@yahoo.com>
2018-10-25 18:50:46 -0700

    Fixed recipe adding and removed extra comments

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import json
-import itertools
 
 import flask
 import dataset
@@ -40,84 +39,6 @@ def startup():
 
 api.add_resource(resources.RecipeList, "/recipe")
 api.add_resource(resources.Recipe, "/recipe/<recipe_id>")
-
-# @app.route("/recipe", methods=['GET'])
-# def get_recipes_top_10():
-    # """Gets the top 10 recipes."""
-    # if not is_authorized():
-        # return flask.Response("{'not': 'happening'}", status=401, mimetype='application/json')
-
-    # # Can we persist the db handle? Tried to persist in Flask.g didn't seem to
-    # # work but could be user error
-    # db = dataset.connect('sqlite:///recipe.db')
-    # recipes_table = db['recipes']
-    # all_results = recipes_table.all()
-    # # Get 10
-    # results_list = [r for r in itertools.islice(all_results, 10)]
-    # return flask.jsonify(results_list)
-
-# @app.route("/recipe/<recipe_id>", methods=['GET'])
-# def get_recipe_by_id(recipe_id):
-    # """Gets one recipe by its recipe id."""
-    # if not is_authorized():
-        # return flask.Response("{'not': 'happening'}", status=401, mimetype='application/json')
-
-    # db = dataset.connect('sqlite:///recipe.db')
-    # recipes_table = db['recipes']
-
-    # one_result_in_list = recipes_table.find(id=recipe_id)
-    # result = [r for r in itertools.islice(one_result_in_list, 1)][0]
-    # print(result)
-    # return flask.jsonify(result)
-
-# @app.route("/recipe", methods=["POST"])
-# def create_recipe():
-    # """Creates a recipe based on receieved parameters and adds it to the db."""
-    # if not is_authorized():
-        # return flask.Response("{'not': 'happening'}", status=401, mimetype='application/json')
-    # if not flask.request.is_json:
-        # flask.abort(400)
-    # print("Creating recipe")
-    # incoming_recipe = flask.request.get_json()
-
-    # # For now assumes full_content
-    # recipe_created = add_recipe_to_db(full_content=incoming_recipe)
-    # if not recipe_created:
-        # flask.abort(500)
-
-    # return flask.Response("Created", status=201, mimetype='application/json')
-
-def add_recipe_to_db(*args, **kwargs):
-    """Adds a recipe to the db."""
-
-    print('Adding recipe to db')
-    db = dataset.connect('sqlite:///recipe.db')
-    recipes_table = db['recipes']
-
-    if "full_content" in kwargs:
-        # generate all fields as in kwargs["full_content"].
-        # use this to just copy from scraped data already formatted in schema.
-        r = kwargs["full_content"]
-
-        # I'm unsure of recipes_table.insert return value
-        db_result = recipes_table.insert(dict(
-            name=r['name'],
-            recipeIngredient=json.dumps(r['recipeIngredient']),
-            recipeYield=r['recipeYield'],
-            recipeInstructions=json.dumps(r['recipeInstructions']),
-        ))
-
-        # db_result is 1 following successful insert but I don't know what it returns on fail
-        # Assuming makes an ass out of you and me
-        return True
-    else:
-        # populate user settable fields from form and auto generate the rest.
-        # e.g. no initial rating, no comments.
-        pass
-
-def is_authorized():
-    """Checks if the user is authorized."""
-    return True
 
 sample_recipe = """
  {
--- resources.py
+++ resources.py
@@ -1,4 +1,5 @@
 import itertools
+import json
 
 import flask
 import flask_restful

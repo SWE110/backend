3604a4486e82871d65e8d6feb215ea20fc53fe85
Tonexus <ual_ynot@yahoo.com>
2018-11-28 22:07:49 -0800

    Added crawler as API path

--- crawler.py
+++ crawler.py
@@ -2,36 +2,45 @@ import sys
 import time
 import queue
 import json
+import threading
 
 import requests
 import bs4
 
-class Crawler:
-    def __init__(self, base_url, seed_pages=[], crawl_count=50, time_delay=0.1, recipe_callback=None, verbose=False):
+class Crawler(threading.Thread):
+    def __init__(self,
+                 base_url,
+                 seed_pages=[""],
+                 crawl_count=50,
+                 time_delay=0.1,
+                 verbose=False,
+                 recipe_callback=None,
+                 recipe_callback_args=(),
+                 recipe_callback_kwargs={}):
         """Sets up the crawler object with some default args"""
+        threading.Thread.__init__(self)
         self.base_url = base_url
         self.pages_to_crawl = queue.Queue()
-        if seed_pages:
-            for page in seed_pages:
-                self.pages_to_crawl.put(page)
-            self.pages_seen = set(seed_pages)
-        else:
-            self.pages_to_crawl.put("")
-            self.pages_seen = set("")
+        for page in seed_pages:
+            self.pages_to_crawl.put(page)
+        self.pages_seen = set(seed_pages)
         self.crawl_count = crawl_count
         self.time_delay = time_delay
         self.recipe_callback = recipe_callback
+        self.recipe_callback_args = recipe_callback_args
+        self.recipe_callback_kwargs = recipe_callback_kwargs
         self.verbose = verbose
         self.recipes = []
+        self.start()
 
     def run(self):
         """Runs the crawler"""
         count = 0
         while count < self.crawl_count and not self.pages_to_crawl.empty():
-            self.v_print("Crawling page {} of {}".format(str(count), str(self.crawl_count)))
+            self.v_print("Crawling page {} of {}".format(count, self.crawl_count))
             self.crawl_next()
             count = count + 1
-            self.v_print("Sleeping for {} seconds".format(str(self.time_delay)))
+            self.v_print("Sleeping for {} seconds".format(self.time_delay))
             sys.stdout.flush()
             time.sleep(self.time_delay)
 
@@ -43,16 +52,20 @@ class Crawler:
             self.v_print("Crawl queue is empty")
             return False
         self.v_print("Full url: {}".format(self.base_url + page_name))
+
+        recipes = get_recipes_from_url(self.base_url + page_name)
+        self.v_print("Found {} recipes for a total of {} recipes".format(len(recipes), len(self.recipes)))
+        self.recipes += recipes
+        if self.recipe_callback is not None:
+            # Runs the recipe callback with args for each recipe found
+            for recipe in recipes:
+                self.recipe_callback(recipe, *(self.recipe_callback_args), **(self.recipe_callback_kwargs))
+
         links = get_links_from_url(self.base_url + page_name)
         # Filters out links not from the base_url
         allowed_links = [link for link in links if link[:len(self.base_url)] == self.base_url]
-        recipes = get_recipes_from_url(self.base_url + page_name)
-        self.v_print("Found {} recipes".format(len(recipes)))
-        self.recipes += recipes
-        if self.recipe_callback is not None:
-            for recipe in recipes:
-                recipe_callback(recipe)
         for link in allowed_links:
+            # Records pages to crawl later, if not already seen
             page = link[len(self.base_url):]
             if page not in self.pages_seen:
                 self.pages_to_crawl.put(page)
